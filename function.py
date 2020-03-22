c4432e49b154d9c939eaba64b392ac0580e8144c
Ayisedo <ajayi.anwansedo@gmail.com>
2020-03-22 07:18:27 -0500

    commit

---
+++ function.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+def square():
+    qazi_turtle = turtle.Turtle()
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+    qazi_turtle.right(90)
+    qazi_turtle.forward(100)
+
+
+
+square()
